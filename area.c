11eaa1dcfaa4c90cc79a4e67dd62360db13c48de
Athira-unni13 <athiramvunnikrishnan1310@gmail.com>
2024-10-25 13:53:00 +0530

    My first commit

---
+++ area.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+  int n;
+  int area;
+  printf("Enter the length of the square: ");
+  scanf("%d", &n);
+  area = n*n;
+  printf("The area of the square is: %d", area);
+}
